2b7dfbbaef45623837d9d44d81afeb996843d6c9
brimstone-art <149525088+brimstone-art@users.noreply.github.com>
2024-04-28 16:53:20 +0300

    коммит коммита еще одного коммита

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,18 @@
-     const gamesRouter = require("express").Router(); 
-     const { deleteGame, sendAllGames, addGameController } = require("../controllers/games")
-     const { getAllGames } = require("../middlewares/games")
+const gamesRouter = require("express").Router(); 
+const { sendAllGames, sendUpdatedGames } = require("../controllers/games")
+const { 
+     getAllGames,
+     checkIsTitleInArray,
+     updateGamesArray,
+     updateGamesFile,
+     findGameById,
+     deleteGame
+} = require("../middlewares/games")
 
-     gamesRouter.post("/games",getAllGames, addGameController)
-     gamesRouter.get("/games",getAllGames, sendAllGames)
-     gamesRouter.delete("/games/:id",getAllGames, deleteGame);
-     
-     module.exports = gamesRouter;
+gamesRouter.get("/games", getAllGames, sendAllGames)
+
+gamesRouter.delete("/games/:id", getAllGames,findGameById, deleteGame, updateGamesFile, sendUpdatedGames);
+
+gamesRouter.post("/games", getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdatedGames)
+
+module.exports = gamesRouter;
